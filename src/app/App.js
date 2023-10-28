909404dabcf40c471d8de0cf2c41806796553cba
Avaz <elonarm21@gmail.com>
2023-10-28 21:59:07 +0500

    r-1 project finished

--- src/app/App.js
+++ src/app/App.js
@@ -8,10 +8,14 @@ import Movies from "../movies/Movies";
 const App = () => {
     return (
         <div className="app font-monospace">
+            <h1 className="py-3 mb-4 text-center text-danger bg-dark">Keeper app</h1>
             <KeeperApp/>
-            <TimeFunc />
-            <MovieProps />
-            <Movies />
+            <h1 className="py-3 my-4 text-center text-danger bg-dark">Time functionality</h1>
+            <TimeFunc/>
+            <h1 className="py-3 my-4 text-center text-danger bg-dark">Movie props</h1>
+            <MovieProps/>
+            <h1 className="py-3 my-4 text-center text-danger bg-dark">Movies</h1>
+            <Movies/>
         </div>
     );
 };
